9fefac76c5c90d1b64e57ea107eca7917c7e39eb
Viraj Tarade <virajtarade1603@gmail.com>
2026-08-10 22:06:34 +0530

    Time: 0 ms (100%), Space: 7.9 MB (12.71%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n<=0)
+         return false; 
+    
+    while (n>1)
+        {
+            if(n%2!=0)
+             return false;
+            n=n/2;
+        }
+         return true;
+        
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
