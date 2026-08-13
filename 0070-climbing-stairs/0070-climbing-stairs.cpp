018b308aa5000fd2fcfca826e89b75fa606d0486
Viraj Tarade <virajtarade1603@gmail.com>
2026-08-13 21:38:03 +0530

    Time: 0 ms (100%), Space: 7.9 MB (59.19%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+      
+        if (n <= 2) {
+            return n;
+        }
+        
+        int prev2 = 1; 
+        int prev1 = 2; 
+        int current = 0;
+        
+        
+        for (int i = 3; i <= n; ++i) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        
+        return prev1;
+    }
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
